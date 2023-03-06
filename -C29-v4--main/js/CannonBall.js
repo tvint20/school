2028c966f084db458b28a7383b550ed51a7787e2
tvint20 <121455348+tvint20@users.noreply.github.com>
2023-03-06 15:24:20 -0300

    Add files via upload

---
+++ -C29-v4--main/js/CannonBall.js
@@ -0,0 +1,37 @@
+class CannonBall {
+  constructor(x, y) {
+    var options = {
+      restitution: 0.8,
+      friction: 1.0,
+      density: 1.0,
+      isStatic: true
+    };
+    this.r = 40;
+//crie um corpo circular
+this.body =Bodies.circle(x,y,this.r, options)
+//carregue a imagem
+this.image=loadImage("assets/cannonball.png")
+    World.add(world, this.body);
+  }
+
+  shoot() {
+    
+    //defina o ângulo da bala para o canhão
+    //defina valor estático para o corpo
+    //defina a velocidade da bala para fazer a bala se mover
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+//mostrar a imagem
+image(this.image,0,0,this.r,this.r)
+    pop();
+
+    }
+  }
+
